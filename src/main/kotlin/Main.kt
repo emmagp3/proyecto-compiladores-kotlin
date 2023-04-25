a344d9c6bc0d73eac1852ecf7f362e19f5ae646f
Emmanuel Valentin <ema_110@outlook.es>
2023-04-25 16:38:19 -0600

    Se agregaron prototipos de funciones a implementar en Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,3 +2,15 @@ fun main(args: Array<String>) {
   val scanner = Scanner("Test")
   scanner.scanTokens().forEach(::println)
 }
+
+
+// TODO: Implementar
+fun run() {}
+
+fun runPrompt() {}
+
+fun runFile() {}
+
+fun error() {}
+
+fun report() {}
